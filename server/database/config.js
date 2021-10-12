bc8e5ee6dd8a3c05d5f4fa757de0fb439c83f3a7
Rehan <m.rd7447@gmail.com>
2021-10-12 10:08:06 -0700

    lab 5 done, using GET to go to /api page from form

---
+++ server/database/config.js
@@ -0,0 +1,23 @@
+export default {
+  development: {
+    username: 'ubuntu',
+    password: 'Veracrypt@12!',
+    database: 'Dining_Hall_Tracker',
+    host: '34.201.154.38',
+    dialect: 'mysql'
+  },
+  test: {
+    username: 'ubuntu',
+    password: 'Veracrypt@12!',
+    database: 'Dining_Hall_Tracker',
+    host: '34.201.154.38',
+    dialect: 'mysql'
+  },
+  production: {
+    username: 'ubuntu',
+    password: 'Veracrypt@12!',
+    database: 'Dining_Hall_Tracker',
+    host: '34.201.154.38',
+    dialect: 'mysql'
+  }
+};
